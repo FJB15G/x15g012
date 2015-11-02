72c7d4dda8e4b1c963bb23deb28eb89d5fbc9ec7
unknown <x15g012@PC504N032.chiba-fjb.ac.jp>
2015-11-02 14:52:54 +0900

    104を追加

---
+++ src/jp/ac/chibafjb/x15g012/kd104/ReAssignment104.java
@@ -0,0 +1,19 @@
+package jp.ac.chibafjb.x15g012.kd104;
+
+public class ReAssignment104 {
+
+	public static void main(String[] args) {
+
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお ");
+		System.out.println("あいうえお ");
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお ");
+		System.out.println("あいうえお ");
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお ");
+
+	}
+
+}
