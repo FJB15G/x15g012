aee9d9ec09f461f86e894857bbe2cb8697350675
unknown <x15g012@PC504N032.chiba-fjb.ac.jp>
2015-11-09 13:51:36 +0900

    115,116を追加

--- src/jp/ac/chibafjb/x15g012/kd115/ReAssignment115.java
+++ src/jp/ac/chibafjb/x15g012/kd115/ReAssignment115.java
@@ -3,7 +3,14 @@ package jp.ac.chibafjb.x15g012.kd115;
 public class ReAssignment115 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	
+		int i;
+		for(i = 1; i <= 2; i++)
+			System.out.println("あいうえお　あいうえお　あいうえお");
+		for(i = 1; i <= 2; i++)
+			System.out.println("かきくけこ　かきくけこ　かきくけこ");
+		for(i = 1; i <= 2; i++)
+			System.out.println("さしすせそ　さしすせそ　さしすせそ");
 
 	}
 
--- src/jp/ac/chibafjb/x15g012/kd116/ReAssignment116.java
+++ src/jp/ac/chibafjb/x15g012/kd116/ReAssignment116.java
@@ -3,7 +3,12 @@ package jp.ac.chibafjb.x15g012.kd116;
 public class ReAssignment116 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		
+		int i;
+		for(i = 1; i <= 2; i++){
+			System.out.println("あいうえお　あいうえお　あいうえお");
+			System.out.println("かきくけこ　かきくけこ　かきくけこ");
+			System.out.println("さしすせそ　さしすせそ　さしすせそ");}
 
 	}
 
