316383cd49c4a2279b3c80a6fb8b924bd67c26c8
unknown <x15g012@PC504N032.chiba-fjb.ac.jp>
2015-11-09 13:56:44 +0900

    121-126を作成

---
+++ src/jp/ac/chibafjb/x15g012/kd121/ReAssignment121.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g012.kd121;
+
+public class ReAssignment121 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g012/kd123/ReAssignment123.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g012.kd123;
+
+public class ReAssignment123 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
