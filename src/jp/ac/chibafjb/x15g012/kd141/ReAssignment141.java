6aadf92def5b9feb2893e7e60ec87cd3ce98af4c
unknown <x15g012@PC504N032.chiba-fjb.ac.jp>
2015-11-09 14:50:23 +0900

    141-144を作成

---
+++ src/jp/ac/chibafjb/x15g012/kd141/ReAssignment141.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g012.kd141;
+
+public class ReAssignment141 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g012/kd142/ReAssignment142.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g012.kd142;
+
+public class ReAssignment142 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
